908eaaaf6bd6807851033764910aca5efb20757d
Ayesha049 <1405049as@gmail.com>
2018-08-03 17:36:19 +0600

    push all

---
+++ app/src/main/java/com/ayeshaapp/ayesha/mojadarhomekitchen/MainActivity.java
@@ -0,0 +1,79 @@
+package com.ayeshaapp.ayesha.mojadarhomekitchen;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        display(R.id.nav_camera);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        int idd = menuItem.getItemId();
+                        display(idd);
+
+
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+    }
+
+    private void display(int id)
+    {
+        Fragment ff = null;
+
+        switch (id)
+        {
+            case R.id.nav_camera:
+                ff = new Menu1();
+                break;
+            case R.id.nav_gallery:
+                ff = new Menu2();
+                break;
+            case R.id.nav_manage:
+                ff = new Menu3();
+                break;
+            case R.id.nav_slideshow:
+                break;
+        }
+        if(ff!=null)
+        {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame,ff);
+            ft.commit();
+
+        }
+
+    }
+
+
+}
